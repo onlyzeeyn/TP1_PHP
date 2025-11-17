a5b0807299c6fbd954e1220701618df9d95796a2
zineddine zougari <zineddinezougari07@gmail.com>
2025-11-17 12:00:56 +0100

    Ajouter le deuxieme code

--- 1.2/exercice.php
+++ 1.2/exercice.php
@@ -1,1 +1,13 @@
-
+<?php
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "FooBar\n";
+    } elseif ($i % 3 == 0) {
+        echo "Foo\n";
+    } elseif ($i % 5 == 0) {
+        echo "Bar\n";
+    } else {
+        echo $i . "\n";
+    }
+}
+?>
